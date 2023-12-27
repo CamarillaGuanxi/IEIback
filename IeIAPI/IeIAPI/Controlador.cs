dbaeaa9ebd132c1aa192064a87b8bcfabb08e52e
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2023-12-27 20:59:00 +0100

    api proyect

---
+++ IeIAPI/IeIAPI/Controlador.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IeIAPI
+{
+    [ApiController]
+    [Route("api/datos")]
+    public class DatosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> ObtenerDatos()
+        {
+            // Lógica para obtener datos desde el backend
+            return "Estos son los datos desde el backend";
+        }
+    }
+}
---
+++ IeIAPI/IeIAPI/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace IeIAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowRender",
+                    builder => builder.WithOrigins("https://ieiapi.onrender.com/api/datos")
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowRender");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
+
