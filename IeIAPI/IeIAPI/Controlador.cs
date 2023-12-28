93f3f9010e7d220dfaa1c7aeee7fab3aaf8ae983
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2023-12-28 13:45:50 +0100

    me muero

--- IeIAPI/IeIAPI/Controlador.cs
+++ IeIAPI/IeIAPI/Controlador.cs
@@ -9,22 +9,49 @@ namespace IeIAPI
     [Route("api/datos")]
     public class DatosController : ControllerBase
     {
-        private readonly IeIAPI.DataAccess dataAccess;
         private static string host = "roundhouse.proxy.rlwy.net";
         private static int port = 56581;
         private static string database = "railway";
         private static string user = "root";
         private static string password = "gfG13156CABHDdcf151AbB6F1c212a1C";
-        public DatosController(IConfiguration configuration)
+        private static string connectionString = $"Server={host};Port={port};Database={database};User Id={user};Password={password};CharSet=utf8mb4;";
+         
+
+        public IEnumerable<object> GetYourModels()
         {
-            string connectionString = $"Server={host};Port={port};Database={database};User Id={user};Password={password};CharSet=utf8mb4;";
-            dataAccess = new IeIAPI.DataAccess(connectionString);
+            List<object> result = new List<object>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand("SELECT * FROM Localidad", connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Mapear los datos del lector a un objeto YourModel
+                            var model = new
+                            {
+                                Id = (int)reader["Codigo"],
+                                nombre = (string)reader["Nombre"],
+                                en_provincia = (int)reader["EnProvincia"],
+                            };
+
+                            result.Add(model);
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<object>> Get()
         {
-            var yourModels = dataAccess.GetYourModels();
+            var yourModels = this.GetYourModels();
             return Ok(yourModels);
         }
 
