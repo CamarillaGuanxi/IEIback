fa62da229ca7811561c01f566205d69cc7cf1e94
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2023-12-28 11:08:01 +0100

    sdasdasd

--- IeIAPI/IeIAPI/Program.cs
+++ IeIAPI/IeIAPI/Program.cs
@@ -5,17 +5,18 @@ namespace IeIAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      
+            public static void Main(string[] args)
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-           Host.CreateDefaultBuilder(args)
-               .ConfigureWebHostDefaults(webBuilder =>
-               {
-                   webBuilder.UseStartup<Startup>();
-                   webBuilder.UseUrls("https://ieiapi.onrender.com/api/datos");  // Cambia aquí el puerto y la dirección
-               });
-    }
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+       }
+ 
 }
