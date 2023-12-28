00b3fa1a0694de5216b43f73e06304423e1fa846
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2023-12-28 11:41:50 +0100

    sdafas

--- IeIAPI/IeIAPI/Program.cs
+++ IeIAPI/IeIAPI/Program.cs
@@ -12,7 +12,7 @@ namespace IeIAPI
                 CreateWebHostBuilder(args).Build().Run();
             }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
                         WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseUrls("http://*:5000");
     }
  
