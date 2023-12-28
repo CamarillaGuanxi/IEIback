8f8a184045e046d815e9332514de5a8335d3e36b
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2023-12-28 11:37:55 +0100

    dokfgaoskg ´

--- IeIAPI/IeIAPI/Program.cs
+++ IeIAPI/IeIAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -8,15 +9,11 @@ namespace IeIAPI
       
             public static void Main(string[] args)
             {
-                CreateHostBuilder(args).Build().Run();
+                CreateWebHostBuilder(args).Build().Run();
             }
 
-            public static IHostBuilder CreateHostBuilder(string[] args) =>
-                Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.UseStartup<Startup>();
-                    });
-       }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                        WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseUrls("http://*:5000");
+    }
  
 }
