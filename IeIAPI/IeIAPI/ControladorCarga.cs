8a6631d28255373ab60cef2455d6adeb75da51b9
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-04 13:47:57 +0100

    Porquye los json si y los otros no

--- IeIAPI/IeIAPI/ControladorCarga.cs
+++ IeIAPI/IeIAPI/ControladorCarga.cs
@@ -40,15 +40,15 @@ namespace IeIAPI
                     numeros[2] = 0; // Corregidos
                     Console.WriteLine("\n-------------------------------");
                     Console.WriteLine("Inicio de extraccion 1");
-                    /*string jsonFilePath = "./resultadoXML.json";
+                    string jsonFilePath = "./resultado.json";
                     string json = System.IO.File.ReadAllText(jsonFilePath);
-                    */
-                      string filePath = Path.Combine(Directory.GetCurrentDirectory(), "IeIAPI", "IeIAPI", "CV.csv");
+                    
+                      //string filePath = Path.Combine(Directory.GetCurrentDirectory(), "IeIAPI", "IeIAPI", "CV.csv");
 
 
                     // Leer el archivo CSV
-                    string[] lines = System.IO.File.ReadAllLines(filePath);
-                    string json = Extractor1CSV.Extractor1(numeros, lines);
+                    //string[] lines = System.IO.File.ReadAllLines(filePath);
+                    //string json = Extractor1CSV.Extractor1(numeros, lines);
                     Console.WriteLine("data" + json);
                     try
                     {
