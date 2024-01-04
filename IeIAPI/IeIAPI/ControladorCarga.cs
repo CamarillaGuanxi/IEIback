54e7296b1440a0504c095d7972a9e6707aa7d073
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-04 02:24:04 +0100

    Magmaou

--- IeIAPI/IeIAPI/ControladorCarga.cs
+++ IeIAPI/IeIAPI/ControladorCarga.cs
@@ -18,7 +18,7 @@ namespace IeIAPI
         private static string password = "HA2A2baGAEH2B1f-4A42b1g6c2EbGaB4";
         private static string connectionString = $"Server={host};Port={port};Database={database};User Id={user};Password={password};CharSet=utf8mb4;";
 
-        [HttpGet]
+        [HttpPost]
         [Route("CSV")]
         public ActionResult<IEnumerable<object>> PostDatos()
         {
@@ -33,13 +33,19 @@ namespace IeIAPI
                     numeros[2] = 0; // Corregidos
                     Console.WriteLine("\n-------------------------------");
                     Console.WriteLine("Inicio de extraccion 1");
-                   // numeros = Extractor1CSV.Extractor1(numeros, connection);
+                    numeros = Extractor1CSV.Extractor1(numeros, connection);
                 }
 
                 return Ok(new { Mensaje = "Datos procesados desde la ruta 'api/carga/CSV'" });
             }
             catch (Exception ex)
             {
+                // Imprimir detalles de la excepción en la consola
+                Console.WriteLine($"Error en el método PostDatos: {ex.GetType().FullName}");
+                Console.WriteLine($"Mensaje de la excepción: {ex.Message}");
+                Console.WriteLine($"StackTrace: {ex.StackTrace}");
+
+                // Retornar un código de estado 500 con un mensaje de error genérico
                 return StatusCode(500, $"Internal Server Error: {ex.Message}");
             }
         }
