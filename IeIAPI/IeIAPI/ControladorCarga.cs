b6f364431ca89fd100a589a563502fdae06c5d72
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-04 22:00:56 +0100

    sdfasdfsdfasdfsdfasdf

--- IeIAPI/IeIAPI/ControladorCarga.cs
+++ IeIAPI/IeIAPI/ControladorCarga.cs
@@ -268,7 +268,7 @@ public IActionResult ProcesarDatos([FromBody] string lines)
                 //Console.WriteLine("Error inserting into Centro_Educativo: " + data.C_E.nombre + " " + ex.Message);
             }
         }
-        [HttpDelete]
+        [HttpGet]
         [Route("BorrarDatos")]
         public IActionResult BorrarDatos()
         {
